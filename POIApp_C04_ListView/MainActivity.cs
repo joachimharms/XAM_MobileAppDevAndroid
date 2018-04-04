8b1a533ecc53f8a8068a6c260b43303f922b854e
joachimharms <joachimharms77@gmail.com>
2018-04-04 13:15:35 +0200

    Add project files.

---
+++ POIApp_C04_ListView/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace POIApp_C04_ListView
+{
+    [Activity(Label = "POIApp_C04_ListView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
